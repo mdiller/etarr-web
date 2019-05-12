ca64ad823c11d4438826030b3ae93a7da00400c9
Malcolm Diller <malcolm@dillerm.io>
2019-05-11 19:28:31 -0700

    fixed quotes, and some parts of the text

--- pages/fees.php
+++ pages/fees.php
@@ -6,7 +6,7 @@
 
 <p>Private pay psychotherapy (55min)  $120.00</p>
 
-<p>Insurance: Psychotherapy may be covered by your medical insurance if it is deemed “medically necessary.”  What this means is that if you are evaluated and meet the criteria for a mental health diagnosis (ie: Anxiety, Depression) your psychotherapy is considered treatment and therefore covered by your medical insurance.  I will complete this evaluation which will involve a clinical diagnosis.  I will bill your medical insurance directly.  You will be responsible for co-pay and deductible payments.  I am in network with the following insurance companies: BCBS, Moda, Cigna, Pacific Source, Aetna, Medicare, First Choice.  All other insurers would be billed as out of network.</p>
+<p>Insurance: Psychotherapy may be covered by your medical insurance if it is deemed "medically necessary."  What this means is that if you are evaluated and meet the criteria for a mental health diagnosis (ie: Anxiety, Depression) your psychotherapy is considered treatment and therefore covered by your medical insurance.  I will complete this evaluation which will involve a clinical diagnosis.  I will bill your medical insurance directly.  You will be responsible for co-pay and deductible payments.  I am in network with the following insurance companies: BCBS, Moda, Cigna, Pacific Source, Aetna, Medicare, First Choice.  All other insurers would be billed as out of network.</p>
 
 <p>HSA (Health Savings Accounts): Often with high deductible medical insurance plans you may have an HSA.  I encourage you to take some time to understand the benefits of this type of plan by visiting <a href="http://www.hsacenter.com/what-is-an-hsa/qualified-medical-expenses/">HSACenter.com</a>.  These accounts can be used to pay for qualified medical expenses including some not covered by your medical insurance. They can be used for co-pay, deductible or out of network expenses. They can also be used for services that are necessary to prevent a mental illness if provided by a mental health professional.  This might include a level of distress or disturbance that does not meet the criteria for a medical insurance eligible diagnosis but should be addressed to alleviate the problem and not further compromise your mental health. </p>
 
@@ -14,5 +14,5 @@
 
 <p>Couple's Counseling: If you are seeking couple's counseling it likely not appropriate to bill your medical insurance.  These services may be eligible  HSA expenses.  If one or both individuals in the couple have a level of distress and disturbance a mental health professional may consider therapy necessary to prevent a mental health illness and would therefore qualify for an eligible HSA expense.</p>
 
-<p>I accept cash and checks, but no credit cards. </p>
+<p>I accept cash, checks, and PayPal, but no credit cards. </p>
 
--- pages/what_to_expect.php
+++ pages/what_to_expect.php
@@ -1,4 +1,4 @@
-<p>Perhaps you've never seen a therapist before or your past experiences were disappointing.  It might be helpful to know what to expect.  We will initially talk on the phone.  I will ask you a few questions to assess whether we are likely to be a good fit.  I will answer any questions you have about me or the work.  If we both agree, we'll set up an initial appointment.  That session will last 50 minutes.</p>
+<p>Perhaps you've never seen a therapist before or your past experiences were disappointing.  It might be helpful to know what to expect.  We will initially talk on the phone.  I will ask you a few questions to assess whether we are likely to be a good fit.  I will answer any questions you have about me or the work.  If we both agree, we'll set up an initial appointment.  That session will last 55 minutes.</p>
 
 <p>At this initial appointment I will ask a lot of questions.  I will also be available to answer questions you might have.  I will share with you my observations and how our work might proceed.  By the end of the initial session, most people are fairly clear about whether they believe this process will be helpful.</p>
 
