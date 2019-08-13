672cb4f1639c979a0852558e00ab18d6fa393c4c
Malcolm Diller <malcolm@dillerm.io>
2019-08-12 19:24:07 -0700

    added latest changes

--- pages/home.php
+++ pages/home.php
@@ -3,9 +3,6 @@
 	<img id="faceimg" src="images/face.jpg" />
 </p>
 <p>
-So you're considering therapy.
-</p>
-<p>
 As a psychotherapist, my goal is to help you experience more success in your life and have an increased sense of well being and self confidence.  We will work together to better understand and navigate the roadblocks that have been undermining your success.  Whether your concerns are personal, professional or involving relationships, it's often helpful to take a fresh look with a professional whose only agenda is to help you find your way forward in a new direction.
 </p>
 <p>
--- pages/services.php
+++ pages/services.php
@@ -1,31 +1,40 @@
-<p>I work with individuals and couples, providing psychotherapy, counseling, coaching, and consultation.</p>
+<p>I work with individuals and couples providing psychotherapy,
+consultation and coaching.</p>
 
-<p>I have been in private practice in the Hillsboro/Beaverton area for <?php echo yearsSince(1997); ?> years.  In that time I have worked with a broad diversity of men and women.  For many, this is their first time seeking therapy.  The decision to seek out help is difficult one for most people.  I appreciate the challenges and awkwardness inherent in this work and try to insure the experience is comfortable and helpful.</p>
+<p>I have maintained a private practice in the Hillsboro/Beaverton area for
+<?php echo yearsSince(1997); ?> years. In that time I have had the pleasure to work with a broad diversity of men and women.</p>
 
-<p>These are some of the situations that might lead someone to seek assistance:</p>
+<p>I provide psychotherapy and consultation/coaching services.</p>
+
+<p>
+	<b>Consultation/Coaching:</b> These services involve assistance for a broad range of issues where the person seeking the services is functioning well and primarily looking for consultation and/or coaching to address a problem. It is often helpful to have the support of trained professional when navigating complex and stressful situations. The type of issues addressed might include:
+</p>
 <ul>
-	<li>Unexpected life events</li>
-	<li>Changes in core relationships</li>
 	<li>Marital frustration and disappointment</li>
-	<li>Career stagnation</li>
-	<li>New child</li>
-	<li>Care for ailing partner or family member</li>
+	<li>Career development</li>
+	<li>Workplace conflict</li>
+	<li>Major life transitions</li>
 	<li>Health related issues for self or partner</li>
-	<li>Work related conflicts</li>
+	<li>Parenting concerns</li>
+	<li>Extended family issues</li>
+	<li>Care for an ailing partner or family member</li>
 </ul>
 
-<p>Treatment specialization areas:</p>
+<p>Consultation and coaching services are typically private pay.</p>
+
+<p><b>Psychotherapy:</b> Psychotherapy typically, but not always, involves treatment for a mental health condition (e.g. Anxiety, Depression). In this case the person seeking services, maybe experiencing symptoms that are distressing and interfering with their daily functioning.</p>
+
+<p>Initially we work to reduce the symptoms (e.g. anxiety, depression) and then to understand and address the underlying patterns that have contributed to the development of the problem. Our goal is an increased sense of mastery and control over how you live your life.</p>
+
+<p>Treatment specialization areas include:</p>
 <ul>
-	<li>Depression and anxiety</li>
-	<li>Relationship issues</li>
-	<li>Family stress</li>
+	<li>Anxiety</li>
+	<li>Depression</li>
 	<li>Trauma history</li>
 	<li>Abuse history</li>
 	<li>Grief issues</li>
 	<li>ADHD</li>
 	<li>Anger issues</li>
-	<li>Stress management</li>
-	<li>Career Development</li>
-	<li>Parental anxiety</li>
-	<li>Couple's counseling</li>
 </ul>
+
+<p>These services may involve a medical diagnosis and therefore can be billed through your medical insurance.</p>
